0fe5e34e319ad70cddd8cac635c47416acecb6d6
katos4 <gregoharriero@gmail.com>
2020-10-07 12:38:53 +0200

    instalar jquery y bootstrap, añadir primera barra de navegacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var $:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  title = 'Angular social';
+
+  ngOnInit() {
+
+    $('h2').css('color', 'orange');
+
+ }
+
+
 }
