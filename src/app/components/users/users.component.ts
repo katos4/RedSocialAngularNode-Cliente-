ce28379053826b38436b45910fd980e9615e3d2f
katos4 <gregoharriero@gmail.com>
2020-10-13 12:00:47 +0200

    componente de gente

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params} from '@angular/router';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+import { GLOBAL } from '../../services/global';
+
 
 @Component({
   selector: 'app-users',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  public title: string;
+  public identity;
+  public token;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _userService: UserService
+  ) {
+    this.title = "Gente";
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.gettoken();
+   }
+
+  ngOnInit(){
+
+    console.log("componente Gente se ha cargado");
   }
 
 }
