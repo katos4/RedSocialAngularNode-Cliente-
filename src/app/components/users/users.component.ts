ff335490c078f460ba6afda8e3fb3e53d8461d18
katos4 <gregoharriero@gmail.com>
2020-10-14 11:32:39 +0200

    botones de seguir y dejar de seguir. Servicio de follows

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params} from '@angular/router';
 import { User } from '../../models/user';
+import { Follow } from '../../models/follow';
 import { UserService } from '../../services/user.service';
+import { FollowService } from '../../services/follow.service';
 import { GLOBAL } from '../../services/global';
+
 
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
+  providers: [UserService, FollowService]
 })
 export class UsersComponent implements OnInit {
   public title: string;
@@ -22,12 +26,14 @@ export class UsersComponent implements OnInit {
   public pages;
   public users: User[];
   public status: string;
-
+  public follows;
+  public followUserOver;
 
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
-    private _userService: UserService
+    private _userService: UserService,
+    private _followService: FollowService
   ) {
     this.title = "Gente";
     this.url = GLOBAL.url;
@@ -71,9 +77,13 @@ getUsers(page){
       if(!response.users){
         this.status = 'error';
       }else{
+        console.log(response);
         this.total = response.total;
         this.users  = response.users;
         this.pages = response.pages;
+        this.follows = response.users_following;
+
+        console.log(this.follows);
         if(page > this.pages){
           this._router.navigate(['/gente', 1]);
         }
@@ -91,6 +101,55 @@ getUsers(page){
 }
 
 
+mouseEnter(user_id){
+  this.followUserOver = user_id;
+}
 
+mouseLeave(user_id){
+  this.followUserOver = 0;
+}
+
+followUser(followed){
+  var follow = new Follow('', this.identity._id, followed);
+  this._followService.addFollow(this.token, follow).subscribe(
+    response => {
+
+      if(!response.follow){
+        this.status = 'error';
+      }else{
+        this.status = 'success';
+        this.follows.push(followed);
+      }
+    },
+    error => {
+      var errorMessage = <any>error;
+      console.log(errorMessage);
+
+      if(errorMessage != null){
+        this.status = 'error';
+      }
+    }
+    );
+}
+
+
+unfollowUser(followed){
+  this._followService.deleteFollow(this.token, followed).subscribe(
+    response => {
+      var search = this.follows.indexOf(followed);
+      if(search != -1){
+        this.follows.splice(search, 1);
+      }
+    }, 
+    error => {
+      var errorMessage = <any>error;
+      console.log(errorMessage);
+
+      if(errorMessage != null){
+        this.status = 'error';
+      }
+    }
+  );
+}
 
 }
