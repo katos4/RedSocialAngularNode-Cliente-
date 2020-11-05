f512cce56c21326859b252a97c56b8638b551ded
katos4 <gregoharriero@gmail.com>
2020-11-05 14:54:07 +0100

    nuevo modal para publicaciones del perfil y confirmacion del borrado

--- src/app/components/publications/publications.component.ts
+++ src/app/components/publications/publications.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Publication } from '../../models/publications';
+import { Like } from '../../models/like';
 import { GLOBAL } from '../../services/global';
 import { UserService } from '../../services/user.service';
 import { PublicationService } from '../../services/publication.service';
@@ -25,6 +26,10 @@ export class PublicationsComponent implements OnInit {
   public itemsPerPage;
   public publications: Publication[];
   public modalData: any[] = [];
+  public open = false;
+  public open2 = false;
+  public likesArray = [];
+  public userPubArray = [];
   @Input() user: string;
 
 
@@ -32,7 +37,8 @@ export class PublicationsComponent implements OnInit {
     private _route: ActivatedRoute,
     private _router: Router,
     private _userService: UserService,
-    private _publicationService: PublicationService
+    private _publicationService: PublicationService,
+    private _likeService: LikeService
   ) {
     this.title = 'Publicaciones';
     this.identity = _userService.getIdentity();
@@ -47,6 +53,8 @@ export class PublicationsComponent implements OnInit {
     var height = $(window).height();
     $('.loginPage').height(height);
     $('.navbar').removeAttr('hidden');
+
+    this.getLikes();
   }
 
 
@@ -89,15 +97,22 @@ export class PublicationsComponent implements OnInit {
     */
    this.page += 1;
 
-    if(this.page == this.pages){
+   if(this.page == this.pages){
       this.noMore = true;
     }
 
-    this.getPublications(this.user, this.page, true);
+   this.getPublications(this.user, this.page, true);
   }
 
 
   openModal(id, file, userImg, userNick, text, userId){
+    this.open = true;
+    ($('#modalPersonalizado') as any).css('display'​​​​​​​​​​​​​​​​​​​​​​​​​​​,'block');​​​​​​
+    ($('body') as any).css('position', 'static');
+    ($('body') as any).css('height', '100%');
+    ($('body') as any).css('overflow', 'hidden');
+
+
     this.modalData = [];
     this.modalData.push({
       "userId": userId,
@@ -107,13 +122,41 @@ export class PublicationsComponent implements OnInit {
       "nick": userNick,
       "text": text});
     console.log(this.modalData);
+    return false;
 
+  }
+
+  closeModal(){
+    if(this.open){
+      ($('#modalPersonalizado') as any).css('display'​​​​​​​​​​​​​​​​​​​​​​​​​​​,'none');​​​​​​
+      ($('body') as any).css('position', 'inherit');
+      ($('body') as any).css('height', 'auto');
+      ($('body') as any).css('overflow', 'visible');
+      this.open = false;
+    } 
+  }
+
+  openDeleteModal(){
+    ($('#deleteModalConfirmation') as any).css('display'​​​​​​​​​​​​​​​​​​​​​​​​​​​,'block');​​​​​​
+    this.open2 = true;
+  }
+
+  closeDeleteModal(){
+    if(this.open2){
+      ($('#deleteModalConfirmation') as any).css('display'​​​​​​​​​​​​​​​​​​​​​​​​​​​,'none');​​​​​​
+      ($('body') as any).css('position', 'inherit');
+      ($('body') as any).css('height', 'auto');
+      ($('body') as any).css('overflow', 'visible');
+      this.open2 = false;
+    }
   }
 
   deletePublication(id){
     console.log("id de la publicacion " + id);
     this._publicationService.deletePublication(this.token, id).subscribe(
       response => {
+        this.closeDeleteModal();
+        this.closeModal();
         this.getPublications(this.user, 1);
       
       },
@@ -123,6 +166,63 @@ export class PublicationsComponent implements OnInit {
     );
   }
 
+  giveLike(idPub){
+    // console.log('el id de la publicacion es: ' + idPub);
+ 
+     var like = new Like('', idPub, this.identity._id,'');
+     this._likeService.addLike(this.token, like).subscribe(
+       response => {
+         if(response){
+           this.status = 'success';
+           this.likesArray.push(idPub);
+           //console.log(this.likesArray);
+         }else{
+           this.status = 'error';
+         }
+       },
+       error => {
+         console.log(<any>error);
+       }
+     );
+   }
+
+   unlikePublication(idPub){
+ 
+     this._likeService.deleteLike(this.token, idPub).subscribe(
+       response => {
+         var search = this.likesArray.indexOf(idPub);
+         if(search != -1){
+           this.likesArray.splice(search, 1);
+         }
+       },
+       error => {
+         console.log(<any>error);
+       }
+     );
+   }
+
+  getLikes(){
+    let temp;
+    
+    this._likeService.getLikes(this.token).subscribe(
+      response => {
+        temp = response;
+       // console.log(temp.myLikes);
+        temp.myLikes.forEach(like => {
+          this.likesArray.push(like.publication);
+
+          if(!this.userPubArray.includes(like.user)){
+            this.userPubArray.push(like.user);
+          }
+         //console.log(like.publication);
+        });
+
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
 
 //-----  
 }
